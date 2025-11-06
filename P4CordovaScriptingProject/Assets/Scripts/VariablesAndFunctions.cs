13c3ab579bae9f46cfed0ad116e1b21dc22c3723
757297-stack <757297@student.dusd.net>
2025-11-06 11:36:01 -0800

    added to project

---
+++ P4CordovaScriptingProject/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    public int myInt = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // create the multiply by two function
+    int MultiplyByTwo(int number)
+    {  
+        //create a local variable
+        int result;
+
+        //multiplying the number by two
+        result = number * 2;
+
+        //return the value
+        return result;
+    }
+    
+    
+      
+
+}
+    
+
